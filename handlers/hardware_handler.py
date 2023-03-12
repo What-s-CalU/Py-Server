a63da00ff684942e19862ad64527f1d5f1f9a917
FDAapproved <32183732+FDAapproved@users.noreply.github.com>
2023-03-11 21:49:52 -0500

    Add Hardware Support
    Cleaned up the harware py module into one that can be ran as a thread alongside the other modules.
    Comment out this entire file when doing unit tests on non-pi platforms, as the necessary libraries will not exist.

---
+++ handlers/hardware_handler.py
@@ -0,0 +1,109 @@
+'''
+    Interfacing functions for GPIO and other pi-exclusive features. 
+    "test program for interfacing a 16x2 Display with LEDs and buttons "
+'''
+
+#! /usr/bin/env python
+
+
+from time import sleep
+import threading
+import RPi.GPIO as rasp_io
+import drivers
+
+# globals specific to this module. 
+PIN_BTN_1: int = 17
+PIN_BTN_2: int = 27
+PIN_LED_1: int = 4
+PIN_LED_2: int = 22
+
+
+# for long string Parameters:
+# (driver, string to print, number of line to print, number of columns of your display) 
+#Return: This function send to display your scrolling string.
+def long_string(display, text="", num_line=1, num_cols=16):
+        if len(text) > num_cols:
+            display.lcd_display_string(text[:num_cols], num_line)
+            sleep(1)
+            for i in range(len(text) - num_cols + 1):
+                text_to_print = text[i:i+num_cols]
+                display.lcd_display_string(text_to_print, num_line)
+                sleep(1)
+            sleep(1)
+        else:
+            display.lcd_display_string(text, num_line)
+
+
+# webscraper's tasks as a thread (which can be ran/released periodically via main)
+class CALUHardwareManagerThread(threading.Thread):
+       def run(self):
+            # mode and warning flags. 
+            rasp_io.setmode(rasp_io.BCM)
+            rasp_io.setwarnings(False)
+
+            # Load the driver and set it to "display"
+            display = drivers.Lcd() 
+
+            #GPIO connections 
+            #################################################################
+            # set up for first led pin and it's button. 
+            rasp_io.setup (PIN_LED_1, rasp_io.OUT)
+            rasp_io.setup (PIN_BTN_1, rasp_io.IN, pull_up_down=rasp_io.PUD_UP)
+            rasp_io.output(PIN_LED_1, False)
+
+
+            # set up for second led pin and it's button. 
+            rasp_io.setup (PIN_LED_2,rasp_io.OUT)
+            rasp_io.setup (PIN_BTN_2,rasp_io.IN, pull_up_down=rasp_io.PUD_UP)
+            rasp_io.output(PIN_LED_2,False)
+            #################################################################
+
+
+
+            # GPIO events
+            ####################################################################
+            # inline functions for buttons 1 and 2
+            def pin_btn_callback(channel):
+                rasp_io.output(PIN_LED_1, True)
+                sleep(1)
+                rasp_io.output(PIN_LED_1,False)
+
+
+            def pin_btn_callback_clear(channel):
+                rasp_io.output(PIN_LED_2, True)
+                sleep(1)
+                rasp_io.output(PIN_LED_2, False)
+                display.lcd_clear()
+                display.lcd_display_string("clearing", 1) 
+                display.lcd_display_string("screen  ", 2)
+                sleep(2)
+                display.lcd_clear()
+                sleep(1)
+
+            # binds functions to buttons 1 and 2
+            rasp_io.add_event_detect(PIN_BTN_1, rasp_io.FALLING, callback=pin_btn_callback,       bouncetime=200)
+            rasp_io.add_event_detect(PIN_BTN_2, rasp_io.FALLING, callback=pin_btn_callback_clear, bouncetime=200)
+
+
+            #continous loop for LCD display
+            #######################################################################
+            try:
+                while True:
+                    display.lcd_display_string("What's @ CALU!", 1)  # Write line of text to first line of display
+                    sleep(2)
+                    display.lcd_clear()
+                    long_string(display,"upcoming notifications:", 2)  # Write line of text to first line of display
+                    sleep(2)
+                    display.lcd_clear()
+                    sleep(1)
+                    display.lcd_display_string("Notifications:", 1)  # Write line of text to second line of display
+                    sleep(3)                                           # Give time for the message to be read
+                    #long_string(display,"most recent notification was: ", 2)   #might use this as another notification 
+                    #sleep(2)                                           # Give time for the message to be read
+                    display.lcd_clear()                                # Clear the display of any data
+                    sleep(3)          # clear, then loop starts again 	     
+            except KeyboardInterrupt as e:
+                print(e)
+                display.lcd_clear()	 # clear the display 
+                rasp_io.cleanup()	 #clear gpio settings 
+            ######################################################################
