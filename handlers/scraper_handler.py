1a352ad84a73f2a67d75650121651c8326381134
FDAapproved <32183732+FDAapproved@users.noreply.github.com>
2023-04-14 01:57:12 -0400

    Scraper Implementation
    The webscraper module now does a recursive fetch for the descriptions (bodies) of each event. In order to skip redundant data and normalize the information, we use .get_text() and .split(), and skip a pre-determined number of lines within the div.

--- handlers/scraper_handler.py
+++ handlers/scraper_handler.py
@@ -34,11 +34,23 @@ class CALUWebScraperThread(threading.Thread):
                             if event_data != None:
                                 for data in event_data:
                                     if i == 0:
-                                        thread_h.s_print(data)
+                                        thread_h.s_print(data.get_text()+"\n")
                                     elif i == 1:
-                                        thread_h.s_print(data)
+                                        thread_h.s_print(data.get_text()+"\n")
+                                        if(data) != None:
+                                            data_response        = requests.get("https://www.calu.edu" + data.find('a')['href'])
+                                            data_response_parsed = BeautifulSoup(str(data_response.text), 'html.parser')
+                                            data_anchor          = data_response_parsed.find('div', class_='b-band__inner')
+                                            data_body            = data_anchor.get_text().split('\n')
+                                            j = 0
+                                            for data_line in data_body:
+                                                if(j >= 5):
+                                                        print(data_line)
+                                                else:
+                                                    j+=1
+
                                     elif i == 2:
-                                        thread_h.s_print(data)
+                                        thread_h.s_print(data.get_text()+"\n")
                                     else:
                                         thread_h.s_print("Unexpected Value.")
                                     i += 1
